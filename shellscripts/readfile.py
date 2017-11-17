bc883e4274c3449de16f0010d8d85a3f8b27abaf
yogavigneshwaran <yogavigneshwaran@users.noreply.github.com>
2017-11-17 15:58:44 +0530

    test changes

--- shellscripts/readfile.py
+++ shellscripts/readfile.py
@@ -1,6 +1,6 @@
 class FileRead():
   def readFile(self):
-    with open('/home/yoga/huboo/shellscripts/deploy.sh') as file_object:
+    with open('./huboo/shellscripts/deploy.sh') as file_object:
       content = file_object.readlines()
     for line in content:
       print(line);
